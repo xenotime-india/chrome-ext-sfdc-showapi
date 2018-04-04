1a65a2731326cfebc34b08854182dad4e1eaf775
xenotime-india <sandeepkhoj@gmail.com>
2018-04-04 13:20:42 +0530

    init

---
+++ insideExtension/src/scripts/contentscript.js
@@ -0,0 +1,10 @@
+console.log('\'Allo \'Allo! Content script','Xenotime');
+window.onload = function() {
+
+    var re = /.com\/(\w{15})/g;
+    var match = re.exec(window.location.href);
+
+    if(match) {
+      showApiName(match[1]);
+    }
+};
